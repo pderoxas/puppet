efe4491681d2b7eaf9900a7369e19ab19bfb7258
Paolo de Roxas <paolo.deroxas@gmail.com>
2013-12-26 17:41:05 -0500

    Update sdk_spec.rb
    Set mock param values

--- modules/sdk/spec/classes/sdk_spec.rb
+++ modules/sdk/spec/classes/sdk_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'sdk' do
+  #set mock values for parameters
+  let(:params) { {:sdk_platform => 'java', :sdk_version => 'v1.0.0'} }
 
   #make sure it creates the base directory
   it do
