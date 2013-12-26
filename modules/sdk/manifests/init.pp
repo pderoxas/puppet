75a5a675a960353b12ea351b88324d3dd2c96e78
Paolo de Roxas <paolo.deroxas@gmail.com>
2013-12-26 15:38:02 -0500

    Update init.pp
    Move params to sdk class

--- modules/sdk/manifests/init.pp
+++ modules/sdk/manifests/init.pp
@@ -1,5 +1,5 @@
-class sdk ($sdk_platform) {
-  notify {"GEO Location=${geo_location}, Store Number=${store_number}":
+class sdk ($sdk_platform, $sdk_version) {
+  notify {"GEO Location=${geo_location}, Store Number=${store_number}, SDK Platform: $sdk_platform, SDK Version: $sdk_version":
     withpath => true,
   }
 
@@ -13,14 +13,10 @@ class sdk ($sdk_platform) {
     purge   => true,
   }
 
-  notify {"SDK Platform: $sdk_platform ":
-    withpath => true,
-  }
-
   case $sdk_platform {
-      'java':     { Class['sdk::java'] { java_sdk_version => hiera('sdk_version') } }
-      'net':      { Class['sdk::dotnet'] { dotnet_sdk_version => hiera('sdk_version') } }
-      default:    { Class['sdk::java'] { java_sdk_version => hiera('sdk_version') } }
+      'java':     { Class['sdk::java'] { java_sdk_version => $sdk_version, } }
+      'net':      { Class['sdk::dotnet'] { dotnet_sdk_version => $sdk_version, } }
+      default:    { Class['sdk::java'] { java_sdk_version => $sdk_version, } }
     }
 
 }
