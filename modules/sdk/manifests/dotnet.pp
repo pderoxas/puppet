fa12de21b33f18aa4b15cce0d8de996585432c54
Paolo de Roxas <paolo.deroxas@gmail.com>
2013-12-26 14:43:36 -0500

    Update dotnet.pp
    Moved implementation into platform specific class

--- modules/sdk/manifests/dotnet.pp
+++ modules/sdk/manifests/dotnet.pp
@@ -1,12 +1,30 @@
-class sdk::dotnet inherits sdk {
+class sdk::dotnet ($dotnet_sdk_version) inherits sdk  {
   #any .NET specific implementation can go here...
 
-  file { "dotnet_readme":
+  notify {".NET SDK Version: $dotnet_sdk_version ":
+    withpath => true,
+  }
+
+  #binary file
+  $path = "/paypal/dotnet-sdk"
+  file { "dotnetsdkdir":
+    ensure  => "directory",
+    path    => $path,
+    source  => "puppet:///sdk_repo/$dotnet_sdk_version",
+    owner   => "root",
+    group   => "root",
+    recurse => true,
+    purge   => true,
+    require => File['basedir'],
+  }
+
+  #sdk version info file
+  file { "dotnet_sdk_version_file":
     ensure   => "file",
-    path     => "/paypal/sdk/readme.txt",
-    content  => "This is just some .NET specific stuff...",
+    path     => "/paypal/net-sdk/$dotnet_sdk_version.txt",
     owner    => "root",
     group    => "root",
-    require  => File['sdkdir'],
+    require  => File['dotnetsdkdir'],
   }
+
 }
