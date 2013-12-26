4d616d0593b0636238b7a320519d9e23e05227d5
Paolo de Roxas <paolo.deroxas@gmail.com>
2013-12-26 14:11:29 -0500

    Update init.pp
    Move implementation into platform specific classes

--- modules/sdk/manifests/init.pp
+++ modules/sdk/manifests/init.pp
@@ -1,10 +1,7 @@
 class sdk {
-  #get data from hiera
-  $sdk_version=hiera('sdk_version')
- 
-notify {"GEO Location=${geo_location}, Store Number=${store_number}, SDK Version: $sdk_version ":
+  notify {"GEO Location=${geo_location}, Store Number=${store_number}":
     withpath => true,
-}
+  }
 
   #base directory of sdk
   file { "basedir":
@@ -16,28 +13,28 @@ notify {"GEO Location=${geo_location}, Store Number=${store_number}, SDK Version
     purge   => true,
   }
 
-  #binary file
-  $path = "/paypal/sdk"
-  file { "sdkdir":
-    ensure  => "directory",
-    path    => $path,
-    source  => "puppet:///sdk_repo/$sdk_version",
-    owner   => "root",
-    group   => "root",
-    recurse => true,
-    purge   => true,
-    require => File['basedir'],
-  }
+#  #binary file
+#  $path = "/paypal/sdk"
+#  file { "sdkdir":
+#    ensure  => "directory",
+#    path    => $path,
+#    source  => "puppet:///sdk_repo/$sdk_version",
+#    owner   => "root",
+#    group   => "root",
+#    recurse => true,
+#    purge   => true,
+#    require => File['basedir'],
+#  }
 
-  #sdk version info file
-  file { "sdk_version_file":
-    ensure   => "file",
-    path     => "/paypal/sdk/sdk_version.txt",
-    content  => $sdk_version,
-    owner    => "root",
-    group    => "root",
-    require  => File['sdkdir'],
-  }
+#  #sdk version info file
+#  file { "sdk_version_file":
+#    ensure   => "file",
+#    path     => "/paypal/sdk/sdk_version.txt",
+#    content  => $sdk_version,
+#    owner    => "root",
+#    group    => "root",
+#    require  => File['sdkdir'],
+#  }
 
 }
 
