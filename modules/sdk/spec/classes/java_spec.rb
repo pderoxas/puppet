157689973b54ca35345ed3411e868c328a336ce2
Paolo de Roxas <paolo.deroxas@gmail.com>
2013-12-26 17:45:23 -0500

    Update java_spec.rb
    Update java rspec tests

--- modules/sdk/spec/classes/java_spec.rb
+++ modules/sdk/spec/classes/java_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 describe 'sdk::java' do
 
-  #make sure it inherits the base class
-  it { should contain_class('sdk') }
-
-  #make sure it creates the readme file
-  it { should contain_file('java_readme') }
+  it { should contain_file('javasdkdir') }
+  it { should contain_file('sdk_version_file') }
 
 end
